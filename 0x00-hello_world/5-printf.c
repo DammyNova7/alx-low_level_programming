269d7a6b3d9e5cba63469ff21cd243f54b2bb167
DammyNova7 <nwalagad2000@gmail.com>
2022-12-09 02:44:53 +0100

    Added 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/**
+ * main - Entry point
+ * Using printf function
+ * Return: Always 0 (Success)
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\m");
+	return (0);
+}
