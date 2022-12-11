d6f28f4a951c82c7d3be2096dc5cac1fef73661f
DammyNova7 <nwalagad2000@gmail.com>
2022-12-11 15:37:41 +0100

    Added 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+/**
+* main - Entry point of program
+* program prints all numbers of base 10,
+* following by a new line
+* Return: Always 0 (Success)
+*/
+
+#include <stdio.h>
+
+int main(void)
+
+{
+	int i = 0;
+
+	for (; i <= 9; i++)
+
+	printf("%d", i);
+
+	return (0);
+}
