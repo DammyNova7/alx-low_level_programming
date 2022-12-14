b64a4d9eb504156a50d873908b389cb2c3610d8b
DammyNova7 <nwalagad2000@gmail.com>
2022-12-14 16:20:14 +0100

    Created a header file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * program prints characters to stdout
+ * without the use of the C standard libraries and functions
+ * Return: 0
+ */
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+return (0);
+}
