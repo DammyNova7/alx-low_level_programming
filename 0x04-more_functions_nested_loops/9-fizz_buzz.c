d833ec9e4cb85c0c128f71a3240440bd98356a93
DammyNova7 <nwalagad2000@gmail.com>
2022-12-17 05:07:52 +0100

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * program is the fizz-buzz game
- * Return: 0 success
- */
+* main - Entry point
+* program prints the numbers from 1 to 100, followed by a new line
+* but for multiples of three, it prints Fizz instead of the number
+* and for the multiples of five print Buzz
+* for numbers which are multiples of both three and five print FizzBuzz
+* each number or word should be separated by a space
+* Return: 0
+*/
 
 int main(void)
 {
-	int i = 0;
-
-	for (; i < 100; i++)
+int i = 1;
+for (i = 1; i < 100; i++)
 {
-	if (i % 15)
+	if ((i % 3 == 0) && (i % 5 == 0))
 	{
 		printf("FizzBuzz ");
 	}
-	else if (i % 3)
+	else if (i % 3 == 0)
 	{
 		printf("Fizz ");
 	}
-	else if (i % 5)
+	else if (i % 5 == 0)
 	{
 		printf("Buzz ");
 	}
