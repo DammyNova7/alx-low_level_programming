65ff9de5711bc2323fd901c39b2057e930926b7c
DammyNova7 <nwalagad2000@gmail.com>
2023-03-16 21:05:48 +0100

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Program prints text formatted text to standard output
+ *
+ * using puts function
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
