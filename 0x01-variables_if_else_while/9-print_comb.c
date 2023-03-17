aa6c699f3c47629ea8b5664956b0f6195bf03d23
DammyNova7 <nwalagad2000@gmail.com>
2023-03-18 00:10:14 +0100

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,13 +12,14 @@ int main(void)
 {
 	int i = '0';
 
-	while (i <= '9')
+	for (; i <= '9'; i++)
 	{
 		putchar(i);
 		if (i < '9')
+		{
 		putchar(',');
 		putchar(' ');
-		i++;
+		}
 	}
 		putchar('\n');
 
