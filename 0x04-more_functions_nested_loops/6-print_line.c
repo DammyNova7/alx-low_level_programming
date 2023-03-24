f52f3f1ede8015f4134bb631b6e82eeff05b3ca4
DammyNova7 <nwalagaddammy7@gmail.com>
2023-03-23 18:55:05 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,16 +12,9 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
-		if (n > 0)
-		{
-			_putchar(95);
-		}
-		else
-		{
-			_putchar(10);
-		}
+		_putchar(95);
 	}
 		_putchar(10);
 }
