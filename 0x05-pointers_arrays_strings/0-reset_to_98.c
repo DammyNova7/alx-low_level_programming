5437e14f64d86cb27c2b83d9e8a7861904d9ae88
DammyNova7 <nwalagaddammy7@gmail.com>
2023-03-27 15:18:05 -0700

    little fix made between directories

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - akes a pointer to an int as parameter and
+ * updates the value it points to to 98
+ *
+ * @n: int to update
+ *
+ * Return: nothing.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
