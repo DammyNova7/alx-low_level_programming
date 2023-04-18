2349bbca72504bb4c5e24fba2e6f3b0713381e47
Dammynova7 <nwalagad2000@gmail.co>
2023-04-18 19:08:07 +0100

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - a newly defined type
+ *
+ * @name: first element
+ * @age: second element
+ * @owner: third element
+ */
+
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog;
+} dog;
 
 #endif
