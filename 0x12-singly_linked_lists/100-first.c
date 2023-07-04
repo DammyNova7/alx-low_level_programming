4510aeac5a156889cd9ae78b1d694b616e04baf7
Dammynova7 <nwalagad2000@gmail.co>
2023-07-04 20:23:13 +0100

    Modified the 100-first.c file

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -10,5 +10,6 @@
 
 void prints(void)
 {
-printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
 }
