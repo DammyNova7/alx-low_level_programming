76a88877a99d1ea4bd344a65220022a25b2300d2
Dammynova7 <nwalagad2000@gmail.co>
2023-07-13 00:40:06 +0100

    Updated 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,8 +18,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	int fd, write_b;
 	int len = 0;
 
-	if (!text_content)
-	return (-1);
+	if (text_content == NULL)
+	return (0);
 
 
 	while (text_content[len] != '\0')
