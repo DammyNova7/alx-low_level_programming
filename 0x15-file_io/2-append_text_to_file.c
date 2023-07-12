2581d67169e159c4d34d4e125db08e353e86c1df
Dammynova7 <nwalagad2000@gmail.co>
2023-07-13 00:26:42 +0100

    Updated 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -22,34 +22,33 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	else
+
+
+	while (text_content[len] != '\0')
 	{
-		while (text_content[len] != '\0')
-		{
-			len++;
-		}
+		len++;
+	}
 
-		if (access(filename, F_OK | W_OK) == 0)
-		{
-			fd = open(filename, O_WRONLY | O_APPEND);
-			if (fd == -1)
-			{
-				return (-1);
-			}
-
-			write_b = write(fd, text_content, len);
-			if (write_b == -1)
-			{
-				close(fd);
-				return (-1);
-			}
-			close(fd);
-			return (1);
-		}
-		else
+	if (access(filename, F_OK | W_OK) == 0)
+	{
+		fd = open(filename, O_WRONLY | O_APPEND);
+		if (fd == -1)
 		{
 			return (-1);
 		}
+
+		write_b = write(fd, text_content, len);
+		if (write_b == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+		close(fd);
+		return (1);
+	}
+	else
+	{
+		return (-1);
 	}
 
 }
