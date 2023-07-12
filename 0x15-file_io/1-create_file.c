f7dd5a4d4e90142b7065f02a60ec0ba22addd3d6
Dammynova7 <nwalagad2000@gmail.co>
2023-07-12 18:38:06 +0100

    Updated 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	write_b = write(fd, filename, sizeof(text_content));
+	write_b = write(fd, text_content, sizeof(text_content));
 	if (write_b == -1)
 	{
 		close(fd);
