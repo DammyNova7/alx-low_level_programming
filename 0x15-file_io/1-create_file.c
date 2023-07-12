b7e6da493ac27ce23fe72602cc5a8e8e9413072b
Dammynova7 <nwalagad2000@gmail.co>
2023-07-12 22:35:52 +0100

    Updated 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,20 +26,31 @@ int create_file(const char *filename, char *text_content)
 
 	size = len;
 
-
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	if (fd == -1)
+	if (text_content == NULL)
 	{
-		return (-1);
-	}
-
-	write_b = write(fd, text_content, size);
-	if (write_b == -1)
-	{
+		fd = open(filename, O_RDWR | O_CREAT, 0600);
+		if (fd == -1)
+		{
+			return (-1);
+		}
 		close(fd);
-		return (-1);
 	}
-	close(fd);
+	else
+	{
+		fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+		if (fd == -1)
+		{
+			return (-1);
+		}
+
+		write_b = write(fd, text_content, size);
+		if (write_b == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+		close(fd);
+	}
 
 	return (1);
 }
