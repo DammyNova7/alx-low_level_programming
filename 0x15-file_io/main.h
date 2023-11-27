47010a297111fd90d10540d9a8a8db3f67ada6d1
Dammynova7 <nwalagad2000@gmail.co>
2023-11-27 19:21:43 +0100

    update: main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,23 +8,10 @@
 
 #define BUFFER_SIZE 1024
 
-/**
- *_error - prints an error message to stderr
- * @code: error code to exit with
- * @message: message to display
- * Return: nothing.
- */
-
-void _error(int code, const char *message)
-{
-	fprintf(stderr, "Error: %s\n", message);
-	exit(code);
-}
-
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
+void _error(int code, const char *message);
 
 #endif
