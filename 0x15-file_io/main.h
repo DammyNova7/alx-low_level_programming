76d7380d10536e3066222b5d92d1e762095cb8f6
Dammynova7 <nwalagad2000@gmail.co>
2023-11-27 18:30:44 +0100

    fix: main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,19 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+/**
+ *_error - prints an error message to stderr
+ * @code: error code to exit with
+ * @message: message to display
+ * Return: nothing.
+ */
+
+void _error(int code, const char *message)
+{
+	fprintf(stderr, "Error: %s\n", message);
+	exit(code);
+}
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
