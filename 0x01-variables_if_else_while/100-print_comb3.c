b1a384f48244a4e71b1bc85586bf64a67037c507
Dammynova7 <nwalagad2000@gmail.co>
2023-12-14 23:37:29 +0100

    Add: 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,14 +13,14 @@ int main(void)
 
 	for (i = 48; i < 57; i++)
 	{
-		for(j = 49; j < 58; j++)
+		for (j = 49; j < 58; j++)
 		{
-			if (i == j)
+			if (i == j || i > j)
 				continue;
 
 			putchar(i);
 			putchar(j);
-			
+
 			if (i == 56 && j == 57)
 				break;
 			putchar(',');
