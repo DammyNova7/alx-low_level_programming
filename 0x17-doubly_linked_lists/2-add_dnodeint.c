d7f34b2899c04b0c8f738cb16f8f2605de79550f
Dammynova7 <nwalagad2000@gmail.co>
2024-01-12 18:45:47 +0100

    Fix: Implemented a check for when the list is not empty

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -23,5 +23,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node->n = n;
 	new_node->prev = NULL;
 
+	if (*head != NULL)
+	{
+		(*head)->prev = new_node;
+	}
+
 	return (new_node);
 }
